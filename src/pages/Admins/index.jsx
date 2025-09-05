a475bfbb0671a4c1a34a3c136fe7c1828d1853c4
Bekki <bekzodmahammadov627@gmail.com>
2025-09-05 15:31:06 +0500

    table

--- src/pages/Admins/index.jsx
+++ src/pages/Admins/index.jsx
@@ -1,4 +1,35 @@
+import UTable from '../../components/UTable';
+
+const data = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'johndoe@gmail.com',
+        phone: { number: '911235678', homeNumber: '123' },
+    },
+    {
+        id: 2,
+        name: 'Jane Smith',
+        email: 'johnsmith@gmail.com',
+        phone: { number: '911235678', homeNumber: '123' },
+    },
+];
+
 const Admins = () => {
-    return <div>Admins</div>;
+    return (
+        <div>
+            Admins
+            <br />
+            <br />
+            <UTable
+                numeric
+                data={data}
+                header={['Name', 'Email', 'Phone']}
+                body={['name', 'email', 'phone.number']}
+                handleDelete={row => console.log('Delete', row)}
+                handleEdit={row => console.log('Edit', row)}
+            />
+        </div>
+    );
 };
 export default Admins;
